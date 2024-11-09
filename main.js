4e172253a77905329b2c56256d694bb3c47a7614
jose1717-beep <tochuna1708@gmail.com>
2024-11-09 00:26:01 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,662 @@
+const productos = 
+[
+    {
+        id: "libros-01",
+        titulo: "Acti-Big 1ro de secundaria",
+        imagen: "bio1.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 35
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Big 2do de secundaria",
+        imagen: "bio2.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-03",
+        titulo: "Acti-Big 3ro de secundaria",
+        imagen: "bio3.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-04",
+        titulo: "Acti-Big 4to de secundaria",
+        imagen: "bio4.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-05",
+        titulo: "Acti-Big 5to de secundaria",
+        imagen: "bio5.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-06",
+        titulo: "Acti-Big 6to de secundaria",
+        imagen: "bio6.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-07",
+        titulo: "Acti-Book 1ro de secundaria",
+        imagen: "ingles1.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 36
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Book 2do de secundaria",
+        imagen: "ingles2.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 36
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Book 3ro de secundaria",
+        imagen: "ingles3.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 38
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Book 4to de secundaria",
+        imagen: "ingles4.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Book 5to de secundaria",
+        imagen: "ingles5.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Book 6to de secundaria",
+        imagen: "ingles6.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Soc 1ro de secundaria",
+        imagen: "soc1.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Soc 2do de secundaria",
+        imagen: "soc2.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Soc 3ro de secundaria",
+        imagen: "soc3.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-02",
+        titulo: "Acti-Soc 4to de secundaria",
+        imagen: "soc4.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-03",
+        titulo: "Acti-Soc 5to de secundaria",
+        imagen: "soc5.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-04",
+        titulo: "Acti-Soc 6to de secundaria",
+        imagen: "soc6.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 1ro de secundaria",
+        imagen: "lengua1.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 2do de secundaria",
+        imagen: "lengua2.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 3ro de secundaria",
+        imagen: "lengua3.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 4to de secundaria",
+        imagen: "lengua4.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 5to de secundaria",
+        imagen: "lengua5.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Lit 6to de secundaria",
+        imagen: "lengua6.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Quim 3ro de secundaria",
+        imagen: "quim.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Quim 4to de secundaria",
+        imagen: "quim2.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 40
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Quim 5to de secundaria",
+        imagen: "quim3.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    {
+        id: "libros-0",
+        titulo: "Acti-Quim 6to de secundaria",
+        imagen: "quim4.png",
+        categoria: {
+            nombre: "¡Bienvenidos a los libros mas recomendados!😊",
+            id: "libros"
+        },
+        precio: 45
+    },
+    /////////////////////////////////////////////////////////////////////
+    {
+        id: "cuadernos-01",
+        titulo: "Cuaderno Oficio Empastado",
+        imagen: "1.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 24
+    },
+    {
+        id: "cuadernos-02",
+        titulo: "Cuaderno carta Empastado",
+        imagen: "2.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 19
+    },
+    {
+        id: "cuadernos-03",
+        titulo: "Cuaderno pequeño Empastado",
+        imagen: "3.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 15
+    },
+    {
+        id: "cuadernos-04",
+        titulo: "Cuaderno Oficio Anillado",
+        imagen: "4.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 18
+    },
+    {
+        id: "cuadernos-05",
+        titulo: "Cuaderno Mediano Anillado",
+        imagen: "55.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 15
+    },
+    {
+        id: "cuadernos-06",
+        titulo: "Cuaderno Pequeño Anillado",
+        imagen: "6.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 12
+    },
+    {
+        id: "cuadernos-07",
+        titulo: "Cuaderno Engrampado",
+        imagen: "7.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 10
+    },
+    {
+        id: "cuadernos-08",
+        titulo: "Cuaderno de Notas",
+        imagen: "8.jpg",
+        categoria: {
+            nombre: "¡Variedad de Cuadernos!🤗",
+            id: "cuadernos"
+        },
+        precio: 10
+    },
+    /////////////////////////////////////////////////////////////////////
+    {
+        id: "otros-01",
+        titulo: "Isocola Grande",
+        imagen: "isocolagrande.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id: "otros"
+        },
+        precio: 10
+    },
+    {
+        id: "otros-02",
+        titulo: "Isocola Mediana",
+        imagen: "isocolamediana.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id: "otros"
+        },
+        precio: 8
+    },
+    {
+        id:"otros-03",
+        titulo: "Isocola Pequeña",
+        imagen: "isocolapequeña.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id: "otros"
+        },
+        precio: 3
+    },
+    {
+        id:"otros-04",
+        titulo: "Colores de 24 Colores",
+        imagen: "colores24.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 26
+    },
+    {
+        id:"otros-05",
+        titulo: "Colores de 12 Colores",
+        imagen: "colores12.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 15
+    },
+    {
+        id:"otros-06",
+        titulo: "Marcadores de 24 Colores",
+        imagen: "marcadorfaber24.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 30
+    },
+    {
+        id:"otros-07",
+        titulo: "Marcadores de 12 Colores",
+        imagen: "marcadorfaber12.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 18
+    },
+    {
+        id:"otros-08",
+        titulo: "Tijera Grande",
+        imagen: "tijeragrande.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 8
+    },
+    {
+        id:"otros-09",
+        titulo: "Tijera Pequeña",
+        imagen: "tijeranormal.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 5
+    },
+    {
+        id:"otros-10",
+        titulo: "Tajador Madison",
+        imagen: "tajadormadison.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 3
+    },
+    {
+        id:"otros-11",
+        titulo: "Tajador",
+        imagen: "tajadornormal.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 2
+    },
+    {
+        id:"otros-12",
+        titulo: "Borrador de Miga",
+        imagen: "borradordemiga.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 2
+    },
+    {
+        id:"otros-13",
+        titulo: "Borrador Madison",
+        imagen: "borra.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 3
+    },
+    {
+        id:"otros-14",
+        titulo: "Lápiz Negro",
+        imagen: "lapiz.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 2.5
+    },
+    {
+        id:"otros-15",
+        titulo: "Estuche Geometrico",
+        imagen: "estuche1.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 18
+    },
+    {
+        id:"otros-16",
+        titulo: "Estuche Geometrico",
+        imagen: "estuche2.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 18
+    },
+    {
+        id:"otros-17",
+        titulo: "Corrector de Tinta",
+        imagen: "correctordetinta.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 5
+    },
+    {
+        id:"otros-18",
+        titulo: "Corrector de Cinta",
+        imagen: "correctorcinta.jpg",
+        categoria: {
+            nombre: "¡Utiles escolares que te podria Interesar!",
+            id:"otros"
+        },
+        precio: 8
+    },
+];
+
+const contenedorProductos = document.querySelector("#contenedor-productos");
+const botonesCategorias = document.querySelectorAll(".boton-categoria");
+const tituloPrincipal = document.querySelector("#titulo-principal");
+let botonesAgregar = document.querySelectorAll(".producto-agregar");
+const numerito = document.querySelector("#numerito");
+
+
+botonesCategorias.forEach(boton => boton.addEventListener("click", () => {
+    aside.classList.remove("aside-visible");
+}))
+
+
+function cargarProductos(productosElegidos) {
+
+    contenedorProductos.innerHTML = "";
+
+    productosElegidos.forEach(producto => {
+
+        const div = document.createElement("div");
+        div.classList.add("producto");
+        div.innerHTML = `
+            <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+            <div class="producto-detalles">
+                <h3 class="producto-titulo">${producto.titulo}</h3>
+                <p class="producto-precio">Bs. ${producto.precio}</p>
+                <button class="producto-agregar" id="${producto.id}">Agregar</button>
+            </div>
+        `;
+
+        contenedorProductos.append(div);
+    })
+
+    actualizarBotonesAgregar();
+}
+
+
+botonesCategorias.forEach(boton => {
+    boton.addEventListener("click", (e) => {
+
+        botonesCategorias.forEach(boton => boton.classList.remove("active"));
+        e.currentTarget.classList.add("active");
+
+        if (e.currentTarget.id != "todos") {
+            const productoCategoria = productos.find(producto => producto.categoria.id === e.currentTarget.id);
+            tituloPrincipal.innerText = productoCategoria.categoria.nombre;
+            const productosBoton = productos.filter(producto => producto.categoria.id === e.currentTarget.id);
+            cargarProductos(productosBoton);
+        } else {
+            tituloPrincipal.innerText = "Todos los productos✏️📚";
+            cargarProductos(productos);
+        }
+
+    })
+});
+
+function actualizarBotonesAgregar() {
+    botonesAgregar = document.querySelectorAll(".producto-agregar");
+
+    botonesAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+
+let productosEnCarrito;
+
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+if (productosEnCarritoLS) {
+    productosEnCarrito = JSON.parse(productosEnCarritoLS);
+    actualizarNumerito();
+} else {
+    productosEnCarrito = [];
+}
+
+function agregarAlCarrito(e) {
+
+    Toastify({
+        text: "Producto agregado",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #4b33a8, #785ce9)",
+          borderRadius: "2rem",
+          textTransform: "uppercase",
+          fontSize: ".75rem"
+        },
+        offset: {
+            x: '1.5rem',
+            y: '1.5rem'
+          },
+        onClick: function(){} 
+      }).showToast();
+
+    const idBoton = e.currentTarget.id;
+    const productoAgregado = productos.find(producto => producto.id === idBoton);
+
+    if(productosEnCarrito.some(producto => producto.id === idBoton)) {
+        const index = productosEnCarrito.findIndex(producto => producto.id === idBoton);
+        productosEnCarrito[index].cantidad++;
+    } else {
+        productoAgregado.cantidad = 1;
+        productosEnCarrito.push(productoAgregado);
+    }
+
+    actualizarNumerito();
+
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosEnCarrito));
+}
+
+function actualizarNumerito() {
+    let nuevoNumerito = productosEnCarrito.reduce((acc, producto) => acc + producto.cantidad, 0);
+    numerito.innerText = nuevoNumerito;
+}
